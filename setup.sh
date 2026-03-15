be232d7951cabf846c5ff7507375e033f51330cd
Stephan Simonett <wireless25@me.com>
2026-03-15 23:03:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -48,7 +48,7 @@ echo "Creating shortcuts..."
 cat >> ~/.bashrc << 'EOF'
 
 # $4 PocketCode aliases
-alias opencode-web="opencode web --hostname 0.0.0.0 --port 4096"
+alias opencode-web="opencode web --hostname $(tailscale ip -4) --port 4096"
 
 # Load NVM on login
 export NVM_DIR="$HOME/.nvm"
